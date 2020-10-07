7839e1c8411a045e78c25439f0760c7e46a81d99
bernix-coder <owusubernice.adjoa@gmail.com>
2020-10-07 14:24:13 +0000

    initial

---
+++ src/components/Table.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function Table() {
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Grade</th>
+                    <th>Age</th>
+                    <th>School</th>
+                </tr>
+
+                <tr>
+                    <td>Ama</td>
+                    <td>12</td>
+                    <td>30</td>
+                    <td>Motown</td>
+                </tr>
+
+                <tr>
+                    <td>Ama</td>
+                 
+                    <td>30</td>
+                    <td>Motown</td>
+                </tr>
+
+                <tr>
+                    <td>Ama</td>
+                    <td>12</td>
+                    <td>30</td>
+                    <td>Motown</td>
+                </tr>
+
+                <tr>
+                    <td>Ama</td>
+                    <td>12</td>
+                    <td>30</td>
+                    <td>Motown</td>
+                </tr>
+
+               
+            </table>
+        </div>
+    )
+}
+
+export default Table
